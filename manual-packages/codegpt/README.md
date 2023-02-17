1200c7e906738cf85e25b9722d86a4c26ba3e055
Blaine Mooers <bmooers1@gmail.com>
2023-02-17 05:40:31 -0600

    Add files via upload

---
+++ manual-packages/codegpt/README.md
@@ -0,0 +1,99 @@
+[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+[![JCS-ELPA](https://raw.githubusercontent.com/jcs-emacs/badges/master/elpa/v/codegpt.svg)](https://jcs-emacs.github.io/jcs-elpa/#/codegpt)
+
+<a href="#"><img align="right" src="./etc/logo.png" width="15%"></a>
+# codegpt
+> Use GPT-3 inside Emacs
+
+[![CI](https://github.com/emacs-openai/codegpt/actions/workflows/test.yml/badge.svg)](https://github.com/emacs-openai/codegpt/actions/workflows/test.yml)
+
+This Emacs Code extension allows you to use the official OpenAI API to generate
+code or natural language responses from OpenAI's [GPT-3](https://en.wikipedia.org/wiki/GPT-3)
+to your questions, right within the editor.
+
+<p align="center">
+<img alt="explain" src="./etc/explain.png"/>
+</p>
+
+## 💾 Installation
+
+#### package.el
+
+This package is available from [JCS-ELPA](https://jcs-emacs.github.io/jcs-elpa/).
+Install from these repositories then you should be good to go!
+
+Normall, you don't need to add `(require 'codegpt)` to your confiugration since
+most `codegpt` commands are autoload and can be called without loading the module!
+
+#### use-package
+
+If you are using [use-package](https://www.emacswiki.org/emacs/UsePackage),
+add the following to your `init.el` file:
+
+```elisp
+(use-package codegpt :ensure t)
+```
+
+or with `straight.el`:
+
+```elisp
+(use-package codegpt
+  :straight (codegpt :type git :host github :repo "emacs-openai/codegpt"))
+```
+
+#### Manual installation
+
+Copy all `.el` files in this repository to `~/.emacs.d/lisp` and add the following:
+
+```elisp
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+(require 'codegpt)
+```
+
+## 🔑 Obtaining API key
+
+To use this extension, you will need an API key from OpenAI. To obtain one,
+follow these steps:
+
+1. Go to [OpenAI's website](https://beta.openai.com/account/api-keys). If you
+don't have an account, you will need to create one or sign up using your Google
+or Microsoft account.
+2. Click on the `Create new secret key` button.
+3. Copy the key and paste into the 'API Key' field under the 'openai' custom group settings.
+
+When you create a new account, you receive $18 in free credits for the API which
+you must use in the first 90 days. You can see pricing information
+[here](https://openai.com/api/pricing/). 1000 tokens are about 700 words, and
+you can see the token count for each request at the end of the response in the
+sidebar.
+
+## 🔨 Usage
+
+Highlight or select code using the `set-mark-command`, then do:
+
+```
+M-x codegpt
+```
+
+List of supported commands,
+
+| Commad            | Description                                     |
+|-------------------|-------------------------------------------------|
+| `codegpt`         | The master command                              |
+| `codegpt-custom`  | Write your own instruction                      |
+| `codegpt-doc`     | Automatically write documentation for your code |
+| `codegpt-fix`     | Find problems with it                           |
+| `codegpt-explain` | Explain the selected code                       |
+| `codegpt-improve` | Improve, refactor or optimize it                |
+
+## Contribute
+
+[![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg)](http://makeapullrequest.com)
+[![Elisp styleguide](https://img.shields.io/badge/elisp-style%20guide-purple)](https://github.com/bbatsov/emacs-lisp-style-guide)
+[![Donate on paypal](https://img.shields.io/badge/paypal-donate-1?logo=paypal&color=blue)](https://www.paypal.me/jcs090218)
+[![Become a patron](https://img.shields.io/badge/patreon-become%20a%20patron-orange.svg?logo=patreon)](https://www.patreon.com/jcs090218)
+
+If you would like to contribute to this project, you may either
+clone and make pull requests to this repository. Or you can
+clone the project and establish your own branch of this tool.
+Any methods are welcome!
---
+++ manual-packages/codegpt/codegpt.el
@@ -0,0 +1,190 @@
+;;; codegpt.el --- Use GPT-3 tp help you write code  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023  Shen, Jen-Chieh
+
+;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; URL: https://github.com/emacs-openai/codegpt
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "26.1") (openai "0.1.0"))
+;; Keywords: convenience codegpt
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Use GPT-3 tp help you write code
+;;
+
+;;; Code:
+
+(require 'openai)
+(require 'openai-completion)
+
+(defgroup codegpt nil
+  "Use GPT-3 tp help you write code."
+  :prefix "codegpt-"
+  :group 'comm
+  :link '(url-link :tag "Repository" "https://github.com/emacs-openai/codegpt"))
+
+(defcustom codegpt-focus-p t
+  "If this value is `nil`, do not move focus to output buffer."
+  :type 'boolean
+  :group 'codegpt)
+
+(defconst codegpt-buffer-name "*CodeGPT*"
+  "Buffer name to do completion task.")
+
+(defcustom codegpt-action-alist
+  `(("custom"  . "Write your own instruction")
+    ("doc"     . "Automatically write documentation for your code")
+    ("fix"     . "Find problems with it")
+    ("explain" . "Explain the selected code")
+    ("improve" . "Improve, refactor or optimize it"))
+  "Alist of code completion actions and its' description."
+  :type 'list
+  :group 'codegpt)
+
+;;
+;;; Application
+
+(defmacro codegpt--ask-in-buffer (instruction &rest body)
+  "Insert INSTRUCTION then execute BODY form."
+  (declare (indent 1))
+  `(progn
+     (openai--pop-to-buffer codegpt-buffer-name)  ; create it
+     (openai--with-buffer codegpt-buffer-name
+       (erase-buffer)
+       (insert ,instruction "\n\n")
+       ,@body)))
+
+(defun codegpt--internal (instruction start end)
+  "Do INSTRUCTION with partial code.
+
+The partial code is defined in with the region, and the START nad END are
+boundaries of that region in buffer."
+  (let ((text (string-trim (buffer-substring start end)))
+        (original-window (selected-window)))
+    (codegpt--ask-in-buffer instruction
+      (insert text "\n\n")
+      (openai-completion
+       (buffer-string)
+       (lambda (data)
+         (openai--with-buffer codegpt-buffer-name
+           (openai--pop-to-buffer codegpt-buffer-name)
+           (let* ((choices (openai--data-choices data))
+                  (result (openai--get-choice choices))
+                  (original-point (point)))
+             (insert (string-trim result) "\n")
+             (fill-region original-point (point))))
+         (unless codegpt-focus-p
+           (select-window original-window))))
+      (unless codegpt-focus-p
+        (select-window original-window)))))
+
+;;;###autoload
+(defun codegpt-doc (start end)
+  "Automatically write documentation for your code.
+
+This command is interactive region only, the START and END are boundaries of
+that region in buffer."
+  (interactive "r")
+  (codegpt--internal
+   "Please write the documentation for the following function."
+   start end))
+
+;;;###autoload
+(defun codegpt-fix (start end)
+  "Fix your code.
+
+This command is interactive region only, the START and END are boundaries of
+that region in buffer."
+  (interactive "r")
+  (codegpt--internal
+   "There is a bug in the following function, please help me fix it."
+   start end))
+
+;;;###autoload
+(defun codegpt-explain (start end)
+  "Explain the selected code.
+
+This command is interactive region only, the START and END are boundaries of
+that region in buffer."
+  (interactive "r")
+  (codegpt--internal
+   "What is the following?"
+   start end))
+
+;;;###autoload
+(defun codegpt-improve (start end)
+  "Improve, refactor or optimize your code.
+
+This command is interactive region only, the START and END are boundaries of
+that region in buffer."
+  (interactive "r")
+  (codegpt--internal
+   "Please improve the following."
+   start end))
+
+;;;###autoload
+(defun codegpt-custom (start end)
+  "Do completion with custom instruction.
+
+This command is interactive region only, the START and END are boundaries of
+that region in buffer."
+  (interactive "r")
+  (codegpt--internal
+   (read-string "Instruction: ")
+   start end))
+
+(defun codegept--execute-predefined-template (start end question)
+  "Ask predefined QUESTION for provided region.
+the START and END are boundaries of that region in buffer."
+  (codegpt--internal question start end))
+
+;;;###autoload
+(defun codegpt (start end)
+  "Do completion with OpenAI to your code.
+
+This command is interactive region only, the START and END are boundaries of
+that region in buffer."
+  (interactive "r")
+  (let*
+      ((offset (openai--completing-frame-offset codegpt-action-alist))
+       (action
+        (completing-read
+         "Select completion action: "
+         (lambda (string predicate action)
+           (if (eq action 'metadata)
+               `(metadata
+                 (display-sort-function . ,#'identity)
+                 (annotation-function
+                  . ,(lambda (cand)
+                       (concat (propertize " " 'display `((space :align-to (- right ,offset))))
+                               (cdr (assoc cand codegpt-action-alist))))))
+             (complete-with-action action codegpt-action-alist string predicate)))
+         nil t))
+       ;; XXX: Guess the function name, `codegpt-xxx'
+       (action-fn (intern (format "codegpt-%s" action))))
+    (if (fboundp action-fn)
+        (funcall action-fn start end)
+      ;; Call predefined action from `codegpt-action-alist'
+      (codegept--execute-predefined-template
+       start end
+       (cdr (assoc action codegpt-action-alist))))))
+
+(provide 'codegpt)
+;;; codegpt.el ends here
