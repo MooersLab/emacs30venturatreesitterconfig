4b6344bec07da1a96312196fcf9f1c1397fedf4c
Blaine Mooers <bmooers1@gmail.com>
2023-03-04 04:41:54 -0600

    Add files via upload
    Changed "saves" directory name to "emacs30InitSaves".

--- init.el
+++ init.el
@@ -241,12 +241,11 @@
 (setq
 backup-by-copying t ; don't clobber symlinks
 backup-directory-alist
-'(("." . "~/.saves")) ; don't litter my fs tree
+'(("." . "~/.emacs30InitSaves")) ; don't litter my fs tree
 delete-old-versions t
 kept-new-versions 6
 kept-old-versions 2
 version-control t)
-
 ;; ############################## Basics Configuration ################################
 ;; ==> adjust here
 ;;(setq openai-key "[]")
